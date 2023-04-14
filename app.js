c2d0b4a80d72b561e7813880423cb9a56fa999d8
Theo02-12 <mrcouvelard@gmail.com>
2023-04-14 23:31:38 +0200

    nice

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const content = document.querySelector('.content')
+const content = document.querySelector('.content');
+// const formEdit = document.querySelector('#formEdit');
+// const submitEdit = document.querySelector('#submitEdit');
+// const nameEdit = document.querySelector('#nameEdit');
+// const avisEdit = document.querySelector('#avisEdit');
 
 fetch('http://localhost:8082/api/avis-clients/?populate=*')
     .then(res => res.json())
@@ -25,22 +29,82 @@ fetch('http://localhost:8082/api/avis-clients/?populate=*')
             const editForm = document.createElement('i');
             editForm.classList.add('fa-solid', 'fa-pen-to-square')
 
+            const editInputBtn = document.createElement('button')
+            editInputBtn.classList.add('edit-input')
+            editInputBtn.innerHTML = `<i class="fa-solid fa-pen"></i>`
+
+            const newEditForm = document.createElement('div');
+            newEditForm.id = 'newEditForm'
+            const newInput = document.createElement('input')
+            newInput.id = 'newInput'
+            const newInputAvis = document.createElement('input')
+            newInput.id = 'newInputAvis'
+            const newButton = document.createElement('button');
+            newButton.id = 'newSubmit'
+            
+
+
+            
             btnDelete.append(deleteForm)
             btnEdit.append(editForm)
-            name.append(clienName)
-            review.append(clienReview)
+            name.innerHTML = clienName;
+            review.innerHTML = clienReview;
 
-            div.append(name, review, btnDelete, btnEdit)
+            div.append(name, review, btnDelete, btnEdit, editInputBtn)
             content.append(div)
 
             function showDelete(){
-                if(btnDelete.style.display == 'block'){
+                if(btnDelete.style.display == 'block'  && editInputBtn.style.display == 'block' ){
                     btnDelete.style.display = 'none';
+                    editInputBtn.style.display = 'none'
                 }else{
                     btnDelete.style.display = 'block'
+                    editInputBtn.style.display = 'block'
                 }
             }
             btnEdit.addEventListener('click', showDelete)
+            
+            function showInput(){
+                if(formEdit.style.display == 'flex'){
+                    formEdit.style.display = 'none';
+                }else{
+                    formEdit.style.display = 'flex'
+                }
+                
+            }
+            editInputBtn.addEventListener('click', showInput)
+
+
+            function submitValue(){
+                console.log(avisEdit.value);
+
+                const valueEdit = {
+                    "data":{
+                        "nom": nameEdit.value,
+                        "review": avisEdit.value,
+                    }
+                }
+
+                fetch(`http://localhost:8082/api/avis-clients/${idrev}`, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(valueEdit)
+                }).then(res => res.json())
+                .then(data => {
+                    
+                    console.log(idrev);
+                    console.log(data);
+                    
+                        const newReview = data.data.attributes.review
+                        const newName = data.data.attributes.nom
+                        name.innerHTML = newName
+                        review.innerHTML = newReview
+
+                })
+            }
+            submitEdit.addEventListener('click', submitValue)
 
 
 
